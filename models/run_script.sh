5314f9b29eb3f4a5e6119eaf973ca7e7a890d015
Felix Wolff <felix.wolff2@student.hpi.de>
2018-12-19 09:03:10 +0100

    Code cleanup

--- models/run_script.sh
+++ models/run_script.sh
@@ -3,8 +3,13 @@
 #GPUNO=3
 #LOGFILE=bpic2011
 #FAMILY=sp2
+OUTDIR="/home/felix.wolff2/docker_share/$LOGFILE"
 
-python model_runner.py $FAMILY individual --gpu=$GPUNO --output=/home/felix.wolff2/docker_share/$LOGFILE ../logs/$LOGFILE/
-python model_runner.py $FAMILY grouped --gpu=$GPUNO --output=/home/felix.wolff2/docker_share/$LOGFILE ../logs/$LOGFILE/
-python model_runner.py $FAMILY padded --gpu=$GPUNO --output=/home/felix.wolff2/docker_share/$LOGFILE ../logs/$LOGFILE/
-python model_runner.py $FAMILY windowed --gpu=$GPUNO --output=/home/felix.wolff2/docker_share/$LOGFILE ../logs/$LOGFILE/
+if [ ! -d $OUTDIR ]; then
+  mkdir "$OUTDIR"
+fi
+
+python model_runner.py $FAMILY individual --gpu=$GPUNO --output="$OUTDIR" ../logs/$LOGFILE/
+python model_runner.py $FAMILY grouped --gpu=$GPUNO --output="$OUTDIR" ../logs/$LOGFILE/
+python model_runner.py $FAMILY padded --gpu=$GPUNO --output="$OUTDIR" ../logs/$LOGFILE/
+python model_runner.py $FAMILY windowed --gpu=$GPUNO --output="$OUTDIR" ../logs/$LOGFILE/
